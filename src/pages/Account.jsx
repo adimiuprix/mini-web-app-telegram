562ba2507acf771ad32d82a8eb1dd008a8aabbdb
Adi pranoto <miuprix@gmail.com>
2024-08-28 05:33:51 +0000

    Account telegram render

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,8 +1,11 @@
 import { buttonVariants } from '../components/ui/button'
 import WebApp from '@twa-dev/sdk'
 
-const username = WebApp.initDataUnsafe.user?.username
-console.log(username)
+// Mendapatkan username
+const userName = WebApp.initDataUnsafe.user?.username ?? 'undefined'
+const firstName = WebApp.initDataUnsafe.user?.first_name ?? 'undefined'
+const lastName = WebApp.initDataUnsafe.user?.last_name ?? 'undefined'
+
 function Account() {
 
   return (
@@ -13,9 +16,9 @@ function Account() {
             <div class="p-8 mx-auto mt-10 max-w-screen-md bg-white bg-opacity-10 rounded-2xl border border-opacity-30 shadow-lg backdrop-blur-sm transition-transform duration-300 transform hover:scale-105">
               <div class="text-3xl font-bold text-center text-blue-900">Detail Akun</div>
               <div class="mt-6">
-                <p class="text-lg font-medium text-blue-900"><strong>Nama:</strong> <span class="text-blue-900">{username}</span></p>
-                <p class="mt-4 text-lg font-medium text-blue-900"><strong>Email:</strong> <span class="text-blue-900">adi@miuprix.com</span></p>
-                <p class="mt-4 text-lg font-medium text-blue-900"><strong>Telegram ID:</strong> <span class="text-blue-900">@augom</span></p>
+                <p class="text-lg font-medium text-blue-900"><strong>Username:</strong> <span class="text-blue-900">{userName}</span></p>
+                <p class="mt-4 text-lg font-medium text-blue-900"><strong>Nama depan:</strong> <span class="text-blue-900">{firstName}</span></p>
+                <p class="mt-4 text-lg font-medium text-blue-900"><strong>Nama belakang:</strong> <span class="text-blue-900">{lastName}</span></p>
               </div>
               <div class="flex gap-y-3 gap-x-5 justify-center mt-8 max-sm:flex-col">
                 <a className={buttonVariants({ variant: 'outline', size: 'lg' })} href="https://github.com/adimiuprix/mini-web-app-telegram">
